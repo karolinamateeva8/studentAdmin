e73d7aade50dd8fd907dfca6e2df7e35b3271224
karolinamateeva8 <karolinamateeva@students.ue-varna.bg>
2023-04-01 23:36:37 +0300

    app done

---
+++ StudentAdmin/www/js/index.js
@@ -0,0 +1,12 @@
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+  console.log("Running cordova-" + cordova.platformId + "@" + cordova.version);
+  loadStudents();
+
+  //Picture
+  $("#camera-btn").click(getPicture);
+  $("#addPicture").click(sendPictureRequest);
+}
